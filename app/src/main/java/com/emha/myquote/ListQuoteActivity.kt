013ba19ffe604c46691e919a32e6c895fa1f9294
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-08-26 16:36:15 +0700

    first commit

---
+++ app/src/main/java/com/emha/myquote/ListQuoteActivity.kt
@@ -0,0 +1,98 @@
+package com.emha.myquote
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.emha.myquote.databinding.ActivityListQuoteBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+
+class ListQuoteActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = ListQuoteActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityListQuoteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListQuoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "List of Quotes"
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.listQuotes.setLayoutManager(layoutManager)
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.listQuotes.addItemDecoration(itemDecoration)
+
+        getListQuotes()
+    }
+
+    private fun getListQuotes() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/list"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                // jika koneksi berhasil
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val listQuote = ArrayList<String>()
+
+                val result = String(responseBody!!)
+                Log.d(TAG, result)
+                try {
+                    val jsonArray = JSONArray(result)
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val quote = jsonObject.getString("en")
+                        val author = jsonObject.getString("author")
+                        listQuote.add("\n$quote\n - $author\n")
+                    }
+
+                    val adapter = QuoteAdapter(listQuote)
+                    binding.listQuotes.adapter = adapter
+                } catch (e: Exception) {
+                    Toast.makeText(this@ListQuoteActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                // Jika koneksi gagal
+                binding.progressBar.visibility = View.INVISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(this@ListQuoteActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
